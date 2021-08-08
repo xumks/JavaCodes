2cd1a04f3befe92ec11700376ea362b15ab9466a
anthenxu <anthenxu@gmail.com>
2021-08-08 16:50:06 +0800

    age

--- HelloWorld/src/com/welays/Main.java
+++ HelloWorld/src/com/welays/Main.java
@@ -4,7 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here 编写代码
+
         System.out.println("Hello,World!");
+
+        int age = 30;
+        System.out.println(age);
     }
 }
 
